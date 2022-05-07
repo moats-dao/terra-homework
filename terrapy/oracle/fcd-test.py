7188cd996af3315afaa22fb4b4f43de21a78ca4e
HC Shin <reflex99@gmail.com>
2022-05-07 09:21:58 +0900

    terrapy oracle

--- terrapy/oracle/fcd-test.py
+++ terrapy/oracle/fcd-test.py
@@ -3,6 +3,8 @@ import base64
 import json, urllib
 from urllib.request import Request
 
+import pandas as pd
+
 from terra_sdk.client.lcd.api.tx import CreateTxOptions
 #from terra_sdk.client.localterra import LocalTerra
 from terra_sdk.client.lcd import LCDClient
@@ -10,7 +12,14 @@ from terra_sdk.core.wasm import MsgStoreCode, MsgInstantiateContract, MsgExecute
 from terra_sdk.core.fee import Fee
 from terra_sdk.key.mnemonic import MnemonicKey
 
-url_request = Request("https://fcd.terra.dev/v1/txs/gas_prices", headers=    {"User-Agent": "Mozilla/5.0"})
-live_gas_prices = json.loads(urllib.request.urlopen(url_request).read().decode())
+# qry_url = r"https://fcd.terra.dev/v1/txs/gas_prices"
+# qry_url = r"https://little-icy-sun.terra-mainnet.quiknode.pro/v1/txs/gas_prices"
+qry_url = r"https://fcd.terra.dev/v1/txs?account=terra1tmnqgvg567ypvsvk6rwsga3srp7e3lg6u0elp8"
+# qry_url = r"https://little-icy-sun.terra-mainnet.quiknode.pro/txs?account=terra1tmnqgvg567ypvsvk6rwsga3srp7e3lg6u0elp8"
 
-pp.pprint(live_gas_prices)
+url_request = Request(qry_url, headers=    {"User-Agent": "Mozilla/5.0"})
+res = json.loads(urllib.request.urlopen(url_request).read().decode())
+
+#pp.pprint(res)
+df = pd.json_normalize(res)
+print(df)
